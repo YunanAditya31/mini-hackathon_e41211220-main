4c3c897b9eb4fd69d8e21bd69f8cb180ee2e443a
Joynn <yunanaditya21@gmail.com>
2023-05-19 08:31:42 +0700

    change

--- resources/views/backend/budaya/index.blade.php
+++ resources/views/backend/budaya/index.blade.php
@@ -22,7 +22,7 @@
                         <li class="nav-item">
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                 data-bs-target="#basicModal">
-                                Tambah Budaya
+                                Tambah Hewan
                             </button>
                         </li>
                     </ul>
@@ -47,7 +47,7 @@
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title">Tambah Budaya</h5>
+                                    <h5 class="modal-title">Tambah Hewan</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
@@ -145,7 +145,7 @@
                                             <div class="modal-body">
 
                                                 <div class="form-group mb-3">
-                                                    <label for="nama">Nama Budaya</label>
+                                                    <label for="nama">Nama Hewan</label>
                                                     <input type="text" class="form-control" name="nama" id="nama"
                                                         value="{{ $item->nama }}" required>
                                                 </div>
